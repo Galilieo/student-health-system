68dc0c35d863bd378b9d0ad9b98f92e3fdcf27bd
Tanter <3551699138@qq.com>
2026-04-26 23:41:31 +0800

    删除多余文件

--- src/main/java/com/Compus_health/mapper/HealthRecordMapper.java
+++
@@ -1,7 +0,0 @@
-package com.Compus_health.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.Compus_health.model.entity.HealthRecord;
-
-public interface HealthRecordMapper extends BaseMapper<HealthRecord> {
-}
