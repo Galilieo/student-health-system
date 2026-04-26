8131ed36d88c574e2a47ca1f5db1e0ef70280cad
Tanter <3551699138@qq.com>
2026-04-26 23:41:31 +0800

    删除多余文件

--- src/main/java/com/Compus_health/mapper/DailyCheckInMapper.java
+++
@@ -1,7 +0,0 @@
-package com.Compus_health.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.Compus_health.model.entity.DailyCheckIn;
-
-public interface DailyCheckInMapper extends BaseMapper<DailyCheckIn> {
-}
